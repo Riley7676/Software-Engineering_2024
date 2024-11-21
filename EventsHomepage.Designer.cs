ef7538e2f59ea654d3782ef4ac3925baf54366a7
Riley Humphreys <RH996@student.aru.ac.uk>
2024-11-21 15:45:10 +0000

    Add project files.

---
+++ EventsHomepage.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Software_Engineering_2024
+{
+    partial class EventsHomepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EventsHomepage));
+            this.events_lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.upcoming_events_btn = new System.Windows.Forms.Button();
+            this.request_event_btn = new System.Windows.Forms.Button();
+            this.past_events_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // events_lbl
+            // 
+            this.events_lbl.AutoSize = true;
+            this.events_lbl.BackColor = System.Drawing.Color.Red;
+            this.events_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.events_lbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.events_lbl.Location = new System.Drawing.Point(441, 122);
+            this.events_lbl.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.events_lbl.Name = "events_lbl";
+            this.events_lbl.Size = new System.Drawing.Size(237, 33);
+            this.events_lbl.TabIndex = 6;
+            this.events_lbl.Text = "About our Events";
+            this.events_lbl.Click += new System.EventHandler(this.events_lbl_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(578, 166);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(349, 232);
+            this.label1.TabIndex = 10;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.ImageLocation = "";
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(168, 166);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(378, 232);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(147, 672);
+            this.panel2.TabIndex = 14;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gainsboro;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(32, 405);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 20);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Membership";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(32, 340);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 20);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Event";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(32, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 20);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Chat";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(32, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 20);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(32, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 20);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Dashboard";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(32, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // upcoming_events_btn
+            // 
+            this.upcoming_events_btn.BackColor = System.Drawing.Color.Gainsboro;
+            this.upcoming_events_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upcoming_events_btn.Location = new System.Drawing.Point(691, 48);
+            this.upcoming_events_btn.Name = "upcoming_events_btn";
+            this.upcoming_events_btn.Size = new System.Drawing.Size(236, 52);
+            this.upcoming_events_btn.TabIndex = 7;
+            this.upcoming_events_btn.Text = "Upcoming Events";
+            this.upcoming_events_btn.UseVisualStyleBackColor = false;
+            // 
+            // request_event_btn
+            // 
+            this.request_event_btn.BackColor = System.Drawing.Color.Gainsboro;
+            this.request_event_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.request_event_btn.Location = new System.Drawing.Point(447, 48);
+            this.request_event_btn.Name = "request_event_btn";
+            this.request_event_btn.Size = new System.Drawing.Size(236, 52);
+            this.request_event_btn.TabIndex = 9;
+            this.request_event_btn.Text = "Request Event";
+            this.request_event_btn.UseVisualStyleBackColor = false;
+            // 
+            // past_events_btn
+            // 
+            this.past_events_btn.BackColor = System.Drawing.Color.Gainsboro;
+            this.past_events_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.past_events_btn.Location = new System.Drawing.Point(205, 48);
+            this.past_events_btn.Name = "past_events_btn";
+            this.past_events_btn.Size = new System.Drawing.Size(236, 52);
+            this.past_events_btn.TabIndex = 8;
+            this.past_events_btn.Text = "Past Events";
+            this.past_events_btn.UseVisualStyleBackColor = false;
+            this.past_events_btn.Click += new System.EventHandler(this.Form2_Load);
+            // 
+            // EventsHomepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(949, 670);
+            this.Controls.Add(this.upcoming_events_btn);
+            this.Controls.Add(this.request_event_btn);
+            this.Controls.Add(this.past_events_btn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.events_lbl);
+            this.Controls.Add(this.label1);
+            this.Name = "EventsHomepage";
+            this.Text = "Events Homepage";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label events_lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button upcoming_events_btn;
+        private System.Windows.Forms.Button request_event_btn;
+        private System.Windows.Forms.Button past_events_btn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EventsHomepage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Engineering_2024
+{
+    public partial class EventsHomepage : Form
+    {
+        public EventsHomepage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void events_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void past_events_btn_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
